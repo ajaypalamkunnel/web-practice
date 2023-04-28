3d39d1d0d5bf453df4d45a17f2921eab61c4bcd9
Ajay ps <ajaypalamkunnel45@gmail.com>
2023-04-28 13:55:25 +0530

    calculator UI completed

---
+++ calculator.js
@@ -0,0 +1,13 @@
+function bclick(val)
+{
+    document.getElementById("screen").value += val
+    
+}
+function clearDisplay()
+{
+    document.getElementById("screen").value= ""
+}
+function equalClick()
+{
+    
+}
